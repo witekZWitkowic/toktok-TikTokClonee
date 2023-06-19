5eb6cc04a4d61d48a2c297df4f311488fc4b320a
witekZWitkowic <witoldadamus94@gmail.com>
2023-06-19 14:40:52 +0200

    Logout button and functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ import { createOrGetUser } from '../utils'
 import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
-  const { userProfile, addUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -51,6 +51,16 @@ const Navbar = () => {
                   />
                 </Link>
               )}
+              <button
+                type='button'
+                className='px-2'
+                onClick={() => {
+                  googleLogout();
+                  removeUser();
+                }}
+                >
+                  <AiOutlineLogout color='red' fontSize={21}/>
+                </button>
             </div>
           ) : (
             <GoogleLogin 
