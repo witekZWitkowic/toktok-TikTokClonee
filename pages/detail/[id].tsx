0b797dac2f8419e5574c5d8ce029a32256095982
witekZWitkowic <witoldadamus94@gmail.com>
2023-07-04 16:28:19 +0200

    Video details added + Play on click

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -7,18 +7,71 @@ import { MdOutlineCancel } from 'react-icons/md'
 import { BsFillPlayFill } from 'react-icons/bs'
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
 import axios from 'axios'
+import { Video } from '../../types'
 import { BASE_URL } from '../../utils'
 
+interface IProps {
+  postDetails: Video,
+}
 
-const Detail = () => {
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoClick = () => {
+    if(playing){
+      videoRef.current?.pause();
+      setPlaying(false);
+    } else {
+        videoRef.current?.play();
+        setPlaying(true);
+    }
+  }
+
+  if(!post) return null;
+
   return (
-    <div>Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
+        <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+          <p>
+            <MdOutlineCancel className='text-white text-[35px]'/>
+          </p>
+        </div> 
+        <div className='relative'>
+          <div className='lg:h-[100vh] h-[60vh]'>
+            <video
+              src={post.video.asset.url}
+              ref={videoRef}
+              loop
+              onClick={()=>{onVideoClick()}}
+              className='h-full cursor-pointer'
+            >
+
+            </video>
+          </div>
+          <div className='absolute top-[45%] left-[45%]'>
+            {!playing && (
+              <button onClick={() => {onVideoClick()}}>
+                <BsFillPlayFill className='text-white text-6xl lg:text-8xl'/>
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
 export const getServerSideProps = async (
-  {params: {id}}) => {
-  const {data} = await axios.get(`${BASE_URL}/api/post/{id}`)  
+  {params: {id}}: {params: {id: string}}) => {
+  const {data} = await axios.get(`http://localhost:3000/api/post/${id}`) 
+
+  return {
+    props: {postDetails: data }
+  }
 }
 
 export default Detail
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -62,7 +62,7 @@ const Upload = () => {
         },
         topic: category,
       }
-      await axios.post(`${BASE_URL}/api/post`, document)
+      await axios.post(`http://localhost:3000/api/post`, document)
 
       router.push('/');
     }
