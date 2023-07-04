334f76b6576003fd685e33e3a961778d4bc91cd1
witekZWitkowic <witoldadamus94@gmail.com>
2023-07-04 17:36:29 +0200

    User name added to each detailed vid

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -19,6 +19,7 @@ const Detail = ({ postDetails }: IProps) => {
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
 
   const onVideoClick = () => {
     if(playing){
@@ -36,7 +37,7 @@ const Detail = ({ postDetails }: IProps) => {
     <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
       <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
         <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
-          <p>
+          <p className='cursor-pointer' onClick={() => {router.back()}}>
             <MdOutlineCancel className='text-white text-[35px]'/>
           </p>
         </div> 
@@ -73,7 +74,43 @@ const Detail = ({ postDetails }: IProps) => {
           )}
         </div>
       </div>
-      
+      <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+        <div className='lg:mt-20 mt-10'>    
+          <div className=' px-9 flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+              <div className='md:w-16 md:h-16 w-16 h-16'>
+                  <Link href="">
+                      <>
+                          <Image 
+                          width={62}
+                          height={62}
+                          className='rounded-full'
+                          src={post.postedBy.image}
+                          alt='Profile Photo'
+                          layout='responsive'
+                          />
+                      </>
+                  </Link>
+              </div>
+              <div>
+                  <Link href="/">
+                      <div className='mt-2 flex flex-col gap-2'>
+                          <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                              {post.postedBy.userName} {` `}
+                              <GoVerified className='text-blue-400 text-md' />
+                          </p>
+                          <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>{post.postedBy.userName}</p>
+                      </div>
+                  </Link>
+              </div>
+          </div>
+          <p className='px-10 text-lg text-gray-600'>
+            {post.caption}
+          </p>
+          <div>
+            {}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
